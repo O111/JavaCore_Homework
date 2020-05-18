3c691287e95be6a6784c82f546fbf4037f785cca
O111 <olegvyborov@gmail.com>
2020-05-18 12:24:25 +0300

    Задание_1_2

---
+++ Wall.java
@@ -0,0 +1,22 @@
+package ru.geekbrains.homework;
+
+public class Wall extends Task {
+
+    private static final float MIN_HEIGHT = 0.5f;
+    private static final float MAX_HEIGHT = 6.0f;
+
+    private float height;
+
+    protected Wall() {
+        this.height = (float) (MIN_HEIGHT + Math.random() * (MAX_HEIGHT - MIN_HEIGHT));
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    @Override
+    public boolean isSuccess(Competitor competitor) {
+        return (competitor.canJump >= this.getHeight());
+    }
+}
